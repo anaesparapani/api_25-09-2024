a3e6a60b2dee66d62e7c0e195653421126e6bb18
anaesparapani <anaclaraesparapani@gmail.com>
2024-09-25 11:14:18 -0300

    first commit

---
+++ src/routes/apiRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+
+const { get } = require('..');
+const userController = require("../controllers/userController")
+
+router.post('/user', userController.createUser);
+router.get('/user', userController.getAllUsers);
+router.put('/user', userController.updateUser);
+router.delete('/user/:cpf', userController.deleteUser);
+
+
+module.exports = router
